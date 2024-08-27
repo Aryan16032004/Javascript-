275838507c4f3bcc65bba2f9a21031f9f5a18cec
Aryan16032004 <112162123+Aryan16032004@users.noreply.github.com>
2024-08-27 11:56:44 +0000

    datatypes

--- Basics/conversion.js
+++ Basics/conversion.js
@@ -1,4 +1,4 @@
-let score =33;
+let score ="33abc";
 
 console.log(typeof score);
 console.log(typeof(score));
@@ -6,3 +6,22 @@ console.log(typeof(score));
 let valueInNumber=Number(score);
 
 console.log(typeof valueInNumber);
+console.log(valueInNumber) // NaN
+
+// "33" => 33
+//"33abc"=>NaN
+// true=>1; false=> 0;
+//null=>0
+
+let isloggedIn=1;
+let booleanIsLoggedIn=Boolean(isloggedIn);
+console.log(booleanIsLoggedIn)
+
+// 1=> true; 0=> false;
+//"" => false
+//"hitesh" => true
+
+let somenumber=33
+let stringNumber=String(somenumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
