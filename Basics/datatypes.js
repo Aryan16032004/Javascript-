9add885d29a72e15d590044835ed98d4f7c850a3
Aryan16032004 <112162123+Aryan16032004@users.noreply.github.com>
2024-08-27 11:48:43 +0000

    basics

---
+++ Basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // not allowed as we are using node not browser
+
+//number
+//bigint
+//string
+//boolean
+//null => standalone value
+// undefined =>value  not assigned
+//symbol =>unique;
+
+// object
+
+console.log(typeof 123);
+console.log (typeof null) // object
+console.log(typeof undefined) //undefined
---
+++ Basics/variables.js
@@ -0,0 +1,9 @@
+const accountId=12224 // const means that the value is not allowed to change 
+let email= "aryan@gmail.com" // this is mainly used to intialize the variables
+
+var city="agra" // not used because of block scope and functional scope
+
+accountname="aryan" // it initializes the value but not to be used
+
+console.table([accountId,email,city,accountname]);
+
