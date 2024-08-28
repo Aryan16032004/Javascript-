476e92b114c42c36b9e8ade1ae55d6c46e7e298c
Aryan16032004 <112162123+Aryan16032004@users.noreply.github.com>
2024-08-28 06:10:15 +0000

    Basic day 2

---
+++ Basics/math.js
@@ -0,0 +1,32 @@
+const score=400;
+// console.log(score)
+
+const balance=new Number(100) // it confirms that the input is number
+// console.log(balance) 
+
+// console.log(balance.toString()); // convert to string and then we can use string functions
+
+// console.log(balance.toFixed(2)) // for decimal values
+
+const otherNumber=123.8966
+// console.log(otherNumber.toPrecision(4)) // it used for precision
+
+const hundreds=10000000
+// console.log(hundreds.toLocaleString('en-IN')) // convert value in indian sysytem
+
+//***********************   MATH  ***************** */
+
+//  console.log(Math.abs(-4))
+//  console.log(Math.round(4.6))
+//  console.log(Math.round(4.4))
+//  console.log(Math.ceil(4.2));
+//  console.log(Math.floor(4.8))
+
+ console.log(Math.random())
+
+ const min=10
+ const max=20
+
+ console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
