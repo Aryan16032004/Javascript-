7f333aaadbc2ee778f0589a36906d2b336eca1e5
Aryan16032004 <112162123+Aryan16032004@users.noreply.github.com>
2024-08-28 12:33:29 +0000

    scope

---
+++ Basics_3/scope.js
@@ -0,0 +1,22 @@
+function one(){
+    const username="aryan"
+
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+
+    // console.log(website)
+    two()
+
+}
+ 
+one() 
+
+
+// const addone=function(num){
+//     return num+1
+// }
+// console.log(addone(6))
+
+// another way to declare function by using variable  
